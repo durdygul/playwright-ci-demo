6c580ab3e25b69bc347bd8da7fe975d0af083993
durdygul <b.durdygul@gmail.com>
2026-04-29 22:55:20 +0300

    v3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,7 +18,7 @@ test('get started link', async ({ page }) => {
 });
 
 
-test('get started link', async ({ page }) => {
+test('get started link2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
